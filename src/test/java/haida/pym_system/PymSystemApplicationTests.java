4517710dfe1514f5a0e7c71d720b9c44cb56494b
lhj548362 <392318054@qq.com>
2020-08-28 12:55:04 +0800

    'init_project'

---
+++ src/test/java/haida/pym_system/PymSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package haida.pym_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
